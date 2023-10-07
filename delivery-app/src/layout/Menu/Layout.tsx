33c910f0eeab03ced739106ab063d2e78cab2d72
Aleksandr <smirnow-2012@mail.ru>
2023-10-07 23:37:26 +0300

    location pathname

--- delivery-app/src/layout/Menu/Layout.tsx
+++ delivery-app/src/layout/Menu/Layout.tsx
@@ -1,18 +1,33 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
+import { useEffect } from 'react';
+import cn from 'classnames';
 
 export function Layout() {
+	const location = useLocation();
+	useEffect(() => {
+		console.log(location);
+	}, [location]);
 	return (
 		<div className={styles['layout']}>
 			<div className={styles['sidebar']}>
 				<div className={styles['user']}>
-					<img src="avatar.png" className={styles['avatar']} alt="аватар пользователя" />
+					<img
+						src="avatar.png"
+						className={styles['avatar']}
+						alt="аватар пользователя"
+					/>
 					<div className={styles['name']}>Алекс</div>
 					<div className={styles['email']}>a@ya.ru</div>
 				</div>
 				<div className={styles['menu']}>
-					<Link to="/" className={styles['link']}>
+					<Link
+						to="/"
+						className={cn(styles['link'], {
+							[styles.active]: location.pathname === '/'
+						})}
+					>
 						<img src="/menu-icon.svg" alt="" />
 						Меню
 					</Link>
