67e36d18533bb2418ba69a231107f69162ff7761
Aleksandr <smirnow-2012@mail.ru>
2023-10-07 17:48:33 +0300

    add routing

--- delivery-app/src/App.tsx
+++ delivery-app/src/App.tsx
@@ -1,11 +1,20 @@
-
+import { Route, Routes } from 'react-router-dom';
 import Form from './components/Form/Form';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
 
 function App() {
-
 	return (
 		<>
 			<Form></Form>
+			<div>
+				<a href="/">Меню</a>
+				<a href="/cart">Корзина</a>
+			</div>
+			<Routes>
+				<Route path="/" element={<Menu />} />
+				<Route path="/cart" element={<Cart />} />
+			</Routes>
 		</>
 	);
 }
