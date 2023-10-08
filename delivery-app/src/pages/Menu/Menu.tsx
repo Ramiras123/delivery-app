5266c3dd050a84ad237f273d8683075cac803a28
Aleksandr <smirnow-2012@mail.ru>
2023-10-08 19:32:08 +0300

    isLoading

--- delivery-app/src/pages/Menu/Menu.tsx
+++ delivery-app/src/pages/Menu/Menu.tsx
@@ -9,13 +9,22 @@ import axios from 'axios';
 
 export function Menu() {
 	const [products, setProducts] = useState<Product[]>([]);
+	const [isLoading, setiIsLoading] = useState<boolean>(false);
 
 	const getMenu = async () => {
 		try {
+			setiIsLoading(true);
+			await new Promise<void>((resolve) =>
+				setTimeout(() => {
+					resolve();
+				}, 2000)
+			);
 			const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
 			setProducts(data);
+			setiIsLoading(false);
 		} catch (err) {
 			console.error(err);
+			setiIsLoading(false);
 			return;
 		}
 	};
@@ -30,17 +39,19 @@ export function Menu() {
 				<Search placeholder="Введите блюдо или состав" />
 			</div>
 			<div>
-				{products.map((p) => (
-					<ProductCard
-						key={p.id}
-						id={p.id}
-						title={p.name}
-						description={p.ingredients.join(',')}
-						price={p.price}
-						rating={p.rating}
-						image={p.image}
-					></ProductCard>
-				))}
+				{!isLoading &&
+					products.map((p) => (
+						<ProductCard
+							key={p.id}
+							id={p.id}
+							title={p.name}
+							description={p.ingredients.join(',')}
+							price={p.price}
+							rating={p.rating}
+							image={p.image}
+						></ProductCard>
+					))}
+				{isLoading && <>Загружаю продукты</>}
 			</div>
 		</>
 	);
