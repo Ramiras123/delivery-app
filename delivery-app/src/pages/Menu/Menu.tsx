a21c74053353d56e130a78d796dc82c45ebef53f
Aleksandr <smirnow-2012@mail.ru>
2023-10-08 15:33:29 +0300

    add cart

--- delivery-app/src/pages/Menu/Menu.tsx
+++ delivery-app/src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from '../../components/Headling/Headling';
+import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
 
@@ -8,6 +9,16 @@ export function Menu() {
 			<div className={styles['head']}>
 				<Headling>Меню</Headling>
 				<Search placeholder="Введите блюдо или состав" />
+			</div>
+			<div>
+				<ProductCard
+					id={1}
+					title={'Наслаждение'}
+					description={'Салями, руккола, помидоры, оливки'}
+					price={300}
+					rating={4.5}
+					image={'/cart-item-1.png'}
+				></ProductCard>
 			</div>
 		</>
 	);
