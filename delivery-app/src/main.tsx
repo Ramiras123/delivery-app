d7d567c4826836a9909ca9e1bf89814e6033a305
Aleksandr <smirnow-2012@mail.ru>
2023-10-11 14:25:26 +0300

    add redux

--- delivery-app/src/main.tsx
+++ delivery-app/src/main.tsx
@@ -9,6 +9,8 @@ import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
 import { Login } from './pages/Login/Login.tsx';
 import { Register } from './pages/Register/Register.tsx';
 import { RequireAuth } from './helpers/RequireAuth.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 const Cart = lazy(() => import('./pages/Cart/Cart'));
@@ -74,6 +76,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router}></RouterProvider>
+		<Provider store={store}>
+			<RouterProvider router={router}></RouterProvider>
+		</Provider>
 	</React.StrictMode>
 );
