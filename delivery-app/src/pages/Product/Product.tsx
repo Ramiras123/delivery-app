2ef9b8d3876631ec86b27e75128c706352abd681
Aleksandr <smirnow-2012@mail.ru>
2023-10-13 23:54:14 +0300

    custom page product

--- delivery-app/src/pages/Product/Product.tsx
+++ delivery-app/src/pages/Product/Product.tsx
@@ -1,6 +1,7 @@
 import { Await, useLoaderData } from 'react-router-dom';
 import { Product } from '../../interfaces/product.interface';
 import { Suspense } from 'react';
+import Headling from '../../components/Headling/Headling';
 
 export function ProductElem() {
 	const data = useLoaderData() as { data: Product };
@@ -8,7 +9,30 @@ export function ProductElem() {
 		<>
 			<Suspense fallback={'Загружаю'}>
 				<Await resolve={data.data}>
-					{({ data }: { data: Product }) => <>{data.name}</>}
+					{({ data }: { data: Product }) => (
+						<>
+							<Headling>{data.name}</Headling>
+							<div>
+								<img src={data.image} alt="" />
+							</div>
+							<div>
+								Цена
+								<div>{data.price}</div>
+							</div>
+							<div>
+								Рейтинг
+								<div>{data.rating}</div>
+							</div>
+							<div>
+								Состав:
+								<ul>
+									{data.ingredients.map((ingredient) => (
+										<li>{ingredient}</li>
+									))}
+								</ul>
+							</div>
+						</>
+					)}
 				</Await>
 			</Suspense>
 		</>
