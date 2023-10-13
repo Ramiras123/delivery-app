3642ba7508806d64c12a27eab2312999da7b15d5
Aleksandr <smirnow-2012@mail.ru>
2023-10-13 16:31:23 +0300

    custom success

--- delivery-app/src/pages/Success/Success.tsx
+++ delivery-app/src/pages/Success/Success.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import styles from './Success.module.css';
+
 export function Success() {
-	return <>Success</>;
+	const navigate = useNavigate();
+	return (
+		<div className={styles['success__wrapper']}>
+			<img src="/success-image.png" alt="" />
+			<div className={styles['success__text']}>Ваш заказ успешно оформлен!</div>
+			<Button appearance="big" onClick={() => navigate('/')}>
+				Сделать новый
+			</Button>
+		</div>
+	);
 }
 
 export default Success;
